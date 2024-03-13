a1240c6ea09d8b970969d26af970f6a060a740a7
gavinmoore2121 <gavin.moore2121@gmail.com>
2024-03-13 18:03:42 -0400

    Create reverse_string_in_place.py

---
+++ reverse_string_in_place.py
@@ -0,0 +1,20 @@
+# Given a string, reverse it in place.
+import string
+from typing import List
+
+
+def main():
+    print(reverse_string(['R', 'e', 'v', 'e', 'r', 's', 'e', ' ', 'm', 'e']))
+
+
+def reverse_string(input_string: List[chr]):
+    # Iterate through list of characters and swap first and last.
+    # Time efficiency: O(n). Space efficiency: O(1)
+    for i in range(0, int(len(input_string) / 2)):
+        input_string[i], input_string[-1 - i] = input_string[-1 - i], input_string[i]
+
+    return input_string
+
+
+if __name__ == '__main__':
+    main()
